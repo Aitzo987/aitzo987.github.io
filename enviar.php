e8bef4ca61220d25c913a9d79c6e3ea68db40c82
Aitzo987 <63805797+Aitzo987@users.noreply.github.com>
2020-05-14 19:07:17 +0200

    ULTIMO

---
+++ enviar.php
@@ -0,0 +1,9 @@
+<?php
+  $destino="aitormutilva@hotmail.com";
+  $nombre=$_Post["nombre"];
+  $correo=$_Post["correo"];
+  $mensaje=$_Post["mensaje"];
+  $contenido = "Nombre": " . $nombre" . "\nCorreo" . $correo . "\nMensaje" . $mensaje; 
+  mail($destino, "contacto", $contenido);
+  header("Location:gracias.html")
+?>
